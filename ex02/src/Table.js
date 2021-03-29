e5ede65684a5722ab66df99d4aa1e72aeb32f5e6
unknown <muhamed.webdevelopment@gmail.com>
2021-03-30 00:35:09 +0200

    ex01 ex 02

---
+++ ex02/src/Table.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Larry</td>
+            <td>Page</td>
+          </tr>
+          <tr>
+            <td>Ada</td>
+            <td>Lovelace</td>
+          </tr>
+          <tr>
+            <td>Alan</td>
+            <td>Turing</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
+export default Table;
