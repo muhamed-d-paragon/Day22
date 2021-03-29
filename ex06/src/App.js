99a7f7866787b8fbdeaa4cb08e11edad8d3393a0
unknown <muhamed.webdevelopment@gmail.com>
2021-03-30 01:23:20 +0200

    ex06

---
+++ ex06/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Plant from './Plant';
+function App() {
+  return (
+    <div className="App">
+      <Plant />
+      </div>
+  );
+}
+export default App;
---
+++ ex06/src/Plant.js
@@ -0,0 +1,5 @@
+import React  from "react";
+const Plant = () => {
+  return <h2>This is a Plant Component!</h2>;
+};
+export default Plant;
